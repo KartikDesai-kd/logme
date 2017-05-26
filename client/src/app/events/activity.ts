f4783766c988e01f63d5c83f936402c20bdf60fd
KartikDesai-kd <kartikdesai3@gmail.com>
2017-05-26 11:30:51 +0530

    Add files via upload

---
+++ client/src/app/events/activity.ts
@@ -0,0 +1,6 @@
+export class Activity{
+    username:string;
+    activity:string;
+    loggedin:number;
+    activitytime:Date;
+}
---
+++ client/src/app/events/event.ts
@@ -0,0 +1,6 @@
+export class Event{
+    username:string;
+    activity:string;
+    loggedin:number;
+    activitytime:Date;
+}
