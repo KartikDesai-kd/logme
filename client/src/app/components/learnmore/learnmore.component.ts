07087a51720aed8fe950b5a8e9637757fb780cdc
KartikDesai-kd <kartikdesai3@gmail.com>
2017-05-26 11:27:45 +0530

    Add files via upload

---
+++ client/src/app/components/learnmore/learnmore.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId:module.id,
+  selector: 'learnmore',
+  templateUrl:'learnmore.component.html'
+
+})
+
+export class LearnmoreComponent  {
+        
+}
